bdd4b5bf271a4767908d25614fb26e469936c2ae
Biberkatze <89131189+Biberkatze@users.noreply.github.com>
2023-12-14 20:12:24 +0100

    Add files via upload

--- M294/app.js
+++ M294/app.js
@@ -1,35 +1,54 @@
 let rezepte = [];
 
+const authServerUrl = "http://localhost:2941/auth"; 
+const ressourceServerUrl = "http://localhost:2940/api/v1/entities";
+
+
+
+
+const app_element = document.getElementById("app");
 const app_el = document.getElementById("app");
 
 const addRezept = function (event) {
-  event.preventDefault();
-  let title = document.querySelector("input[name='rezept']").value;
-  let rezept = { title: title };
+	event.preventDefault();
+	let title = document.querySelector("input[name='rezept']").value;
+	let rezept = { title: title };
+  
+	fetch("http://localhost:2940/api/v1/entities", {
+	  method: "POST",
+	  headers: { "content-type": "application/json" },
+	  body: JSON.stringify(rezept),
+	})
+	  .then((response) => response.json())
+	  .then((json) => {
+		// Check if the server response contains the expected data
+		if (json && json.id) {
+		  rezept.id = json.id;
+  
+		  rezepte.push(rezept);
+		  updateList(); // Update the displayed list after adding a new item
+  
+		  // After adding a new item, fetch the updated list
+		  fetch("http://localhost:2940/api/v1/entities")
+			.then((response) => response.json())
+			.then((data) => {
+			  // Assuming data is an array of recipes
+			  rezepte = data;
+			  updateList(); // Update the displayed list with the new data
+			})
+			.catch((error) => {
+			  console.error("Error fetching data:", error);
+			});
+		} else {
+		  console.error("Unexpected server response:", json);
+		}
+	  })
+	  .catch((error) => {
+		console.log(error);
+	  });
+  };
+  
 
-  fetch("http://localhost:2940/api/v1/entities", {
-    method: "POST",
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify(rezept),
-  })
-    .then((response) => response.json())
-    .then((json) => {
-      if (json && json.id) {
-        rezept.id = json.id;
-        rezepte.push(rezept);
-
-        // Save the updated data to localStorage
-        localStorage.setItem("rezepte", JSON.stringify(rezepte));
-
-        updateList();
-      } else {
-        console.error("Unexpected server response:", json);
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
 
 const createForm = function (parent) {
   const form_el = document.createElement("form");
@@ -75,12 +94,16 @@ const addListElement = function (rezept, parent) {
 };
 
 const updateList = function () {
-  // Clear the existing list
-  app_el.innerHTML = "";
+  const ul_el = document.querySelector("#app ul");
+  // Clear the list
+  ul_el.innerHTML = "";
 
-  // Recreate the form and list
-  createForm(app_el);
-  createList(app_el);
+  // Re-populate the list with the updated rezepte array
+  if (Array.isArray(rezepte)) {
+    rezepte.forEach((rezept) => {
+      addListElement(rezept, ul_el);
+    });
+  }
 };
 
 const deleteRezept = function (id) {
@@ -92,9 +115,6 @@ const deleteRezept = function (id) {
       // Remove the deleted rezept from the array
       rezepte = rezepte.filter((rezept) => rezept.id !== id);
 
-      // Save the updated data to localStorage
-      localStorage.setItem("rezepte", JSON.stringify(rezepte));
-
       // Update the displayed list
       updateList();
     })
@@ -102,10 +122,8 @@ const deleteRezept = function (id) {
       console.log(error);
     });
 };
-
-// Initialize the list with data from localStorage
-rezepte = JSON.parse(localStorage.getItem("rezepte")) || [];
-updateList();
+createForm(app_el);
+createList(app_el);
 
 const form_el = document.querySelector("#app form");
 form_el.addEventListener("submit", addRezept);
